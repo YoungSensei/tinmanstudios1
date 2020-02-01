90f60ab100d997b0cc13f228bd00459c22d2cde9
YoungSensei <59657629+YoungSensei@users.noreply.github.com>
2020-02-01 13:14:43 -0500

    manager

--- GameJam/Assets/Box.cs
+++ GameJam/Assets/Box.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class Box : MonoBehaviour
 {
     public bool PowerOn;
+    GameObject gm;
 
     private void Start()
     {
-        PowerOn = false; 
+        PowerOn = false;
+        gm = GameObject.Find("GameManager");
     }
     private void OnTriggerStay(Collider other)
     {
         if (other.tag == "Box")
         {
             other.GetComponent<Renderer>().material.color = Color.blue;
-            
+            gm.GetComponent<GameManager>().powerLevel++;
             PowerOn = true;
         }
     }
