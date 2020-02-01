cd09ae839c322b448e574cd3a9d078ee8906e07d
YoungSensei <59657629+YoungSensei@users.noreply.github.com>
2020-02-01 12:08:36 -0500

    new mechanics added
    push box
    jump and gravity

---
+++ GameJam/Assets/Box.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    public bool PowerOn;
+
+    private void Start()
+    {
+        PowerOn = false; 
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Box")
+        {
+            other.GetComponent<Renderer>().material.color = Color.blue;
+            
+            PowerOn = true;
+        }
+    }
+}
--- GameJam/Assets/Scripts/ThirdPersonController.cs
+++ GameJam/Assets/Scripts/ThirdPersonController.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class ThirdPersonController : MonoBehaviour
 {
-    public float speed;
+    public float speed=10f;
+    public float gravity;
     CharacterController controller;
+    Vector3 velocity;
+    public float force = 3f;
+   
+
+    public float jumpheight=4;
+    public float jumpApex = .4f;
+    float jumpvelocity;
+    float vSpeed;
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        gravity = -(2 * jumpheight) / Mathf.Pow(jumpApex, 2);
+        jumpvelocity = Mathf.Abs(gravity) * jumpApex;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (controller.isGrounded)
+        {
+            vSpeed = 0;
+            if (Input.GetButtonDown("Jump"))
+            {
+                vSpeed = jumpvelocity;
+            }
+        }
         PlayerMovement();
     }
 
@@ -24,5 +43,23 @@ public class ThirdPersonController : MonoBehaviour
         float ver = Input.GetAxisRaw("Vertical");
         Vector3 playerMovement = transform.right * hor + transform.forward * ver;
         controller.Move(playerMovement*speed*Time.deltaTime);
+
+        
+        vSpeed += gravity * Time.deltaTime;
+        velocity.y = vSpeed;
+
+        controller.Move(velocity * Time.deltaTime);
     }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        
+            if (hit.gameObject.tag == "Box")
+            {
+            
+                hit.rigidbody.AddForce(transform.forward * force);
+            }
+        
+    }
+
 }
