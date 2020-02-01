d975d100e4b9fc8e95bc8a05370ce8918c00ac49
Primezurker <codyray1226@gmail.com>
2020-02-01 16:10:54 -0500

    Menus and Button Manager
    merged the main menu and pause menu scripts, made button manager, pause menu, and option menu were made into prefabs.

---
+++ GameJam/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Lv.1");
+    }
+
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public static bool OptionIsEnabled = false;
+    public GameObject optionMenuUI;
+
+    // Update is called once per frame
+
+    public void Enable()
+    {
+        optionMenuUI.SetActive(true);
+        OptionIsEnabled = true;
+    }
+    public void Disable()
+    {
+        optionMenuUI.SetActive(false);
+        OptionIsEnabled = false;
+    }
+
+    public AudioMixer audioMixer;
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("music", volume);
+        audioMixer.SetFloat("sfx", volume);
+    }
+    public void SetMusic(float volume)
+    {
+        audioMixer.SetFloat("music", volume);
+    }
+    public void SetSfx(float volume)
+    {
+        audioMixer.SetFloat("sfx", volume);
+    }
+}
