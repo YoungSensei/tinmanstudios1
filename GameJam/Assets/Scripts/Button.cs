ce68bce97ac0aea24e4c59476d59288ed11416e5
Leifeon001 <tristen.howk@yahoo.com>
2020-02-02 01:21:19 -0500

    Adding Changes to Lvl.1 Creating Materials
    Uploading the Buttons and Scripts

---
+++ GameJam/Assets/Scripts/Button.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public bool PlayerIsHere = false;
+    public bool ButtonPressed = false;
+
+    public Material Bad;
+    public Material Good;
+
+    public GameObject WaterTank;
+    public void Update()
+    {
+        if(ButtonPressed == true)
+        {
+            WaterTank.GetComponent<Renderer>().material.color = Good.color;
+        }
+        else
+        {
+            WaterTank.GetComponent<Renderer>().material.color = Bad.color;
+        }
+        if(PlayerIsHere == true)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                ButtonPressed = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Player")
+        {
+            PlayerIsHere = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.name == "Player")
+        {
+            PlayerIsHere = false;
+        }
+    }
+}
